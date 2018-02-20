6ba2a389496c25af0a2cfa7aa98a2086accb1ff0
Molly Moran <mollyclaremoran@gmail.com>
2018-02-20 12:31:50 -0600

    basic POST request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,11 +5,24 @@ var path = require("path");
 module.exports = function(app) {
     //GET route
     app.get("/api/friends", function(req, res) {
-      res.json(friendData);
+        res.json(friendData);
     });
 
     //POST route
     app.post("/api/friends", function (req, res){
+        var userResponse = req.body;
 
+        // var scoreArr = userResponse.scores;
+        // console.log(scoreArr);
+
+        // var numArr = scoreArr.map(i => parseInt(i, 10));
+        // console.log(numArr);
+
+        // // userResponse.name = req.name;
+        // // userResponse.photo = req.photo;
+        // // userResponse.scores = numArr;
+        // // console.log(userResponse);
+
+        res.json(userResponse);
     })
-}
+};
