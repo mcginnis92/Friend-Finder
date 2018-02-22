4f92c354b7fcf8b7e8a76085b538b9a13302f96f
Molly Moran <mollyclaremoran@gmail.com>
2018-02-21 20:05:40 -0600

    modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,8 @@
 var friends = [
     {
       name: "Russian Blue",
-      photo: "",
+      photo: "images/russian.jpg",
+      description: "Like this breed of cat, you’re content to be alone and don’t demand constant attention. Although you may need to get to know others first, you are loyal to your friends. Your low-key personality makes you a great match for this feline!",
       scores: [
         1, 
         1, 
@@ -20,7 +21,8 @@ var friends = [
 
     {
       name: "Parakeet",
-      photo: "",
+      photo: "images/bird.jpg",
+      description: "Your colorful personality and natural extraversion draw others to you. You’ll be a great match for this intelligent bird, since you both share a love for fruit and spending time with others.",
       scores: [
         5, 
         2, 
@@ -37,7 +39,8 @@ var friends = [
  
     {
       name: "Golden Retriever",
-      photo: "",
+      photo: "images/retriever.jpg",
+      description: "Like this social, playful pup, you’re most content when you’re with others. Others appreciate your solid loyalty and ability to make them smile. Your energy and love for the outdoors align well with this friendly breed.",
       scores: [
         5, 
         5, 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,42 +2,40 @@ var friends = require("../data/friends");
 var path = require("path");
 
 module.exports = function (app) {
-
-  //GET route
-  app.get("/api/friends", function (req, res) {
-    res.json(friends);
-  });
-
   //POST route
   app.post("/api/friends", function (req, res) {
 
     //Turns the score string into an array so we can change the values to numbers
     var userScores = JSON.parse(req.body.scores);
-    var scoreNums = userScores.map(function(i) {
+    var scoreNums = userScores.map(function (i) {
       return parseInt(i, 10);
     });
 
     //Checks the differences in response between the user input and each existing friend
+    var absValue0 = 0;
     var absValue1 = 0;
     var absValue2 = 0;
-    var absValue3 = 0;
 
-        for (var i = 0; i < scoreNums.length; i++) {
-          absValue1 += Math.abs(friends[0].scores[i] - scoreNums[i]);
-          absValue2 += Math.abs(friends[1].scores[i] - scoreNums[i]);
-          absValue3 += Math.abs(friends[2].scores[i] - scoreNums[i]);
-        }
+    for (var i = 0; i < scoreNums.length; i++) {
+      absValue0 += Math.abs(friends[0].scores[i] - scoreNums[i]);
+      absValue1 += Math.abs(friends[1].scores[i] - scoreNums[i]);
+      absValue2 += Math.abs(friends[2].scores[i] - scoreNums[i]);
+    }
 
-        var newFriend;
-        var match = Math.min(absValue1, absValue2, absValue3);
-            if (match == absValue1) {
-              newFriend = "russian blue";
-            } else if (match == absValue2) {
-              newFriend = "parakeet";
-            } else if (match == absValue3) {
-              newFriend = "golden retriever";
-            }
+    var newFriend;
+    var match = Math.min(absValue0, absValue1, absValue2);
+    switch (match) {
+      case absValue0:
+        newFriend = friends[0];
+        break;
+      case absValue1:
+        newFriend = friends[1];
+        break;
+      case absValue2:
+        newFriend = friends[2];
+        break;
+    }
 
-      res.json(newFriend);
+    res.json(newFriend);
   })
 };
